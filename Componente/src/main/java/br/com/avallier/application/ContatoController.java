98a9b8a283603091a4677a72a45770b44081c694
avallier <avallier@uol.com.br>
2015-06-30 11:23:52 -0300

    Atualizacao

---
+++ Componente/src/main/java/br/com/avallier/application/ContatoController.java
@@ -0,0 +1,6 @@
+package br.com.avallier.application;
+
+//@ManagedBean
+public class ContatoController {
+	
+}
---
+++ Componente/src/main/java/br/com/avallier/domain/entity/Contato.java
@@ -0,0 +1,25 @@
+package br.com.avallier.domain.entity;
+
+public class Contato {
+	String nome;
+	String telefone;
+	
+	public Contato()	{
+	
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
